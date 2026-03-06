3761129b7018c987e7e8de02728ef955e872b09a
shaig-mahmudov <zyphorix.official@gmail.com>
2026-03-06 02:52:06 -0800

    Add: Course class

---
+++ src/main/java/com/ironhack/homeworkjavaironbattle/model/Course.java
@@ -0,0 +1,59 @@
+package com.ironhack.homeworkjavaironbattle.model;
+import jakarta.annotation.Nullable;
+
+import java.util.UUID;
+
+public class Course {
+    private UUID uuid = UUID.randomUUID();
+    private String courseId = uuid.toString();
+    private String name;
+    private double price;
+    private double money_earned;
+//    private @Nullable Teacher teacher;
+
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+//
+//    @Nullable
+//    public Teacher getTeacher() {
+//        return teacher;
+//    }
+//
+//    public void setTeacher(@Nullable Teacher teacher) {
+//        this.teacher = teacher;
+//    }
+
+    public double getMoney_earned() {
+        return money_earned;
+    }
+
+    public void setMoney_earned(double money_earned) {
+        this.money_earned = money_earned;
+    }
+}
